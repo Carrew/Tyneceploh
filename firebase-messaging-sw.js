bf328e72503dc118e84d0b227cf42c936994dd20
Carrew <www.sandicarrew@gmail.com>
2026-05-13 01:41:25 +0000

    Refactor Firebase messaging service worker script

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,43 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"
+);
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDjswYVR2ijJLil3hnHlzBq9NLMW5VHVg4",
-  authDomain: "tyneceploh.firebaseapp.com",
-  projectId: "tyneceploh",
-  messagingSenderId: "161024255934"
+
+  apiKey:"YOUR_API_KEY",
+
+  authDomain:"tyneceploh.firebaseapp.com",
+
+  projectId:"tyneceploh",
+
+  messagingSenderId:"YOUR_SENDER_ID",
+
+  appId:"YOUR_APP_ID"
+
 });
 
-const messaging = firebase.messaging();
+const messaging =
+  firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
+messaging.onBackgroundMessage(payload=>{
 
   self.registration.showNotification(
+
     payload.notification.title,
+
     {
-      body: payload.notification.body,
-      icon: "/logo.png"
+
+      body:
+        payload.notification.body,
+
+      icon:"icon.png"
+
     }
+
   );
 
 });
